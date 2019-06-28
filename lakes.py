0f1e91c495e25972302c7cd86742a6dba27a9f8a
hdsingh <harmandeepsinghhds@gmail.com>
2019-06-28 11:13:00 +0530

    Update env

--- lakes.py
+++ lakes.py
@@ -2,7 +2,7 @@ import xrviz
 import xarray as xr
 from xrviz.dashboard import Dashboard
 
-ds = xr.open_dataset('data/great_lakes.nc')
+data = xr.open_dataset('data/great_lakes.nc')
 
-dash = Dashboard(ds)
+dash = Dashboard(data)
 dash.show()
